61ed31bdfe3103c2f31a48448eaf4f7c6fedc7ea
ARP <andrewarp06@gmail.com>
2026-09-16 09:28:50 +0200

    Add files via upload

---
+++ Calculadora2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package a1.calculadora2;
+
+/**
+ *
+ * @author ALUMNOS
+ */
+public class Calculadora2 extends NewJFrame{
+
+    public static void main(String[] args) {
+        new Calculadora2().setVisible(true);
+    }
+}
